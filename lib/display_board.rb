738c32585f2af1c5c7b5a48700046a0641246a9b
Tok Tok <rere055@gmail.com>
2020-08-06 09:51:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+  arr = [" ", " "," "," ", " ", " ", " ", " ", " "]
+def display_board(arr)
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+  arr = [" ", " "," "," ", "X", " ", " ", " ", " "]
+def display_board(arr)
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
